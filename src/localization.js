90f10b3f34a5efcefb23ff5f27063d3e4ad75ba2
Dmitry Bragin <dbragin@aboutecho.com>
2012-06-22 12:11:07 +0400

    Localization component and test for it was fixed

--- src/localization.js
+++ src/localization.js
@@ -2,24 +2,56 @@
 
 if (Echo.Localization) return;
 
-Echo.Localization = { "labels": {} };
+Echo.Localization = function(labels, namespace) {
+	var self = this;
+	this.labels = {};
+	this.namespace = namespace;
+	$.each(labels, function(name, value) {
+		self.labels[Echo.Localization.key(name, self.namespace)] = value;
+	});
+};
+
+Echo.Localization.prototype.label = function(name, data) {
+	var key = Echo.Localization.key(name, this.namespace);
+	return this.labels[key]
+		? Echo.Localization.substitute(this.labels[key], data)
+		: Echo.Localization.label(name, this.namespace, data);
+}
+
+Echo.Localization.prototype.extend = function(labels) {
+	var self = this;
+	$.each(labels, function(name, value) {
+		var key = Echo.Localization.key(name, self.namespace);
+		self.labels[key] = value;
+	});
+}
+
+// static interface
+
+Echo.Localization.labels = { "defaults": {}, "custom": {} };
 
 Echo.Localization.key = function(name, namespace) {
 	return (namespace ? namespace + "." : "") + name;
 };
 
-Echo.Localization.extend = function(labels, namespace) {
+Echo.Localization.extend = function(labels, namespace, isDefault) {
 	$.each(labels, function(name, value) {
-		Echo.Localization.labels[Echo.Localization.key(name, namespace)] = value;
+		var key = Echo.Localization.key(name, namespace);
+		Echo.Localization.labels[isDefault ? "defaults" : "custom"][key] = value;
 	});
 };
 
 Echo.Localization.label = function(name, namespace, data) {
-	var label = Echo.Localization.labels[Echo.Localization.key(name, namespace)] || name;
+	var key = Echo.Localization.key(name, namespace);
+	var label = Echo.Localization.labels["custom"][key] || Echo.Localization.labels["defaults"][key] || name;
+	return Echo.Localization.substitute(label, data);
+};
+
+Echo.Localization.substitute = function(label, data) {
 	$.each(data || {}, function(key, value) {
 		label = label.replace(new RegExp("{" + key + "}", "g"), value);
 	});
 	return label;
-};
+}
 
 })();
--- tests/unit/localization.js
+++ tests/unit/localization.js
@@ -11,14 +11,51 @@ suite.prototype.tests = {};
 
 suite.prototype.tests.TestLocalizationMethods = {
 	"check": function() {
+		
 		Echo.Localization.extend({
 			"field": "value",
 			"key": "{content}"
 		}, "Namespace");
-		QUnit.equal(Echo.Localization.labels["Namespace.field"], "value", "Checking extends() method");
-		QUnit.equal(Echo.Localization.key("field", "Namespace"), "Namespace.field", "Checking key() method");
-		QUnit.equal(Echo.Localization.label("field", "Namespace"), "value", "Checking label() method");
-		QUnit.equal(Echo.Localization.label("key", "Namespace", {"content": "test"}), "test", "Checking label() method with data param");
+		
+		QUnit.equal(Echo.Localization.key("field", "Namespace"), "Namespace.field", "Checking static key() method");
+		QUnit.equal(Echo.Localization.label("field", "Namespace"), "value", "Checking static label() method");
+		QUnit.equal(Echo.Localization.label("wrong_field", "Namespace"), "wrong_field", "Checking static label() method with wrong name param");
+		QUnit.equal(Echo.Localization.label("key", "Namespace", {"content": "test"}), "test", "Checking static label() method with data param");
+		
+		Echo.Localization.extend({
+			"field": "default_value",
+			"key": "default_content"
+		}, "Namespace", 1);
+		
+		QUnit.equal(Echo.Localization.label("field", "Namespace"), "value", "Checking that custom localization is not overriden by default");
+		
+		var TestObject = function(data, namespace) {
+			this.localization = new Echo.Localization(data, namespace);
+		};
+		
+		TestObject.prototype.getField = function(field) {
+			return this.localization.label(field);
+		};
+		
+		TestObject.prototype.getOverridenField = function(field, data) {
+			return this.localization.label(field, data);
+		}
+		
+		TestObject.prototype.extendField = function(data) {
+			this.localization.extend(data);
+		}
+		
+		var localization = {
+			"field": "test_value",
+			"key": "{test_content}"
+		};
+		
+		var object = new TestObject(localization, "Namespace");
+		QUnit.equal(object.getField("field"), "test_value", "Checking label() method of instance");
+		QUnit.equal(object.getOverridenField("key", {"test_content": "new_content"}), "new_content", "Checking label() method of instance with data");
+		object.extendField({"new_key": "new_value"});
+		QUnit.equal(object.getField("new_key"), "new_value", "Checking extend() + label() methods");
+		QUnit.equal(Echo.Localization.label("field", "Namespace"), "value", "Checking that custom global localization is not overriden by instance localization");
 	}
 }
 
