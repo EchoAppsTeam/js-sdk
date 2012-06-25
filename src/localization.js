6de7a15d3bb44cf1fe3f4c81e54967f51ad7daf2
Leonid Mosenkov <leon@aboutecho.com>
2012-06-25 11:47:54 +0400

    Added more inline dox to the lib

--- src/localization.js
+++ src/localization.js
@@ -7,11 +7,12 @@ if (Echo.Localization) return;
 /**
  * Constructor of class encapsulating language variable mechanics.
  *
- * Static methods should be used for general language variables definition.
- * The class should be instanciated to override language variables within the scope of a particular component. 
+ * The class should be instanciated to override language variables within the scope of a particular component instance.
+ * Language variables overriden with the instance of this class will have the highest priority.
+ * Static methods should be used for general language variable definition and localization purposes.
  *
- * @param {Object} labels object containing the list of language variables
- * @param {String} namespace string representing the namespace
+ * @param {Object} labels flat object containing the list of language variables to be initialized
+ * @param {String} namespace string representing the component namespace
  * @api public
  */
 
@@ -25,13 +26,13 @@ Echo.Localization = function(labels, namespace) {
 };
 
 /**
- * Method returning the language variable value by its name.
+ * Method returning the language variable value by its name within the scope of a particular component instance.
  *
  * Function will return the language variable value corresponding to this instance.
- * If current instance doesn't contain this particular language variable, it will fall back to the defined general language variable list.
+ * If current instance doesn't contain this particular language variable, it will fall back to the global language variable list.
  *
  * @param {String} name language variable name
- * @param {Object} data data that should be inserted instead of a placeholder in the language variable
+ * @param {Object} data flat object data that should be inserted instead of a placeholder in the language variable
  * @return {String}
  * @api public
  */
@@ -44,9 +45,9 @@ Echo.Localization.prototype.label = function(name, data) {
 };
 
 /**
- * Method to add/override the language variable list in the scope of current instance.
+ * Method to add/override the language variable list within the scope of a particular component instance.
  *
- * @param {Object} labels object containing the list of language variables
+ * @param {Object} labels flat object containing the list of language variables to be added/overriden 
  * @api public
  */
 
@@ -58,7 +59,21 @@ Echo.Localization.prototype.extend = function(labels) {
 	});
 };
 
-// static interface
+// Static interface
+/**
+ * Function to add/override the language variable list in the global scope.
+ *
+ * Function should be used to define the default language variable list.
+ * In this use case the `isDefault` param should be set to `true`.
+ * Function should also be used for localization purposes.
+ * In this case the `isDefault` param can be omitted or set to `false`.
+ * The values overriden with the function will be available globally.
+ *
+ * @param {Object} labels object containing the list of language variables
+ * @param {String} namespace string representing the namespace
+ * @param {Boolean} isDefault flag switching the localization mode to setting defaults one
+ * @api public
+ */
 
 Echo.Localization.extend = function(labels, namespace, isDefault) {
 	$.each(labels, function(name, value) {
@@ -66,6 +81,20 @@ Echo.Localization.extend = function(labels, namespace, isDefault) {
 		Echo.Localization._labels[isDefault ? "general" : "custom"][key] = value;
 	});
 };
+
+/**
+ * Function returning the language variable value by its name from the global language variable list.
+ *
+ * Function will return the language variable value from the global language variable list.
+ * It also takes into consideration the localized values.
+ * If value of the particular language varibale is not found in the localization list it will fall back to the default language variable value.
+ *
+ * @param {String} name language variable name
+ * @param {String} namespace string representing the namespace
+ * @param {Object} data flat object data that should be inserted instead of a placeholder in the language variable 
+ * @return {String}
+ * @api public
+ */
 
 Echo.Localization.label = function(name, namespace, data) {
 	var key = Echo.Localization._key(name, namespace);
