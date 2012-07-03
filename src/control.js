21e15fe5567683d30ae1c25bd3b0225f26afe150
Andrew Kushnir <kushnir.andrew@gmail.com>
2012-07-03 14:31:11 +0400

    First draft of the Echo.Control class and Auth control on top of it

---
+++ src/control.js
@@ -0,0 +1,163 @@
+(function($) {
+
+"use strict";
+
+if (Echo.Utils.isComponentDefined("Echo.Control")) return;
+
+Echo.Control = function() {};
+
+// static interface
+
+Echo.Control.create = function(manifest) {
+	var control = Echo.Utils.getNestedValue(manifest.name, window);
+	// prevent multiple re-definitions
+	if (control) return control;
+	var _constructor = function(config) {
+		if (!config || !config.target) return;
+		this.manifest = manifest;
+		this.init([
+			"vars",
+			["config", [config]],
+			"events",
+			"labels",
+			"css",
+			"renderers",
+			["user", [manifest.constructor]]
+		]);
+	};
+	Echo.Utils.inherit(_constructor, manifest.inherits || Echo.Control);
+	if (manifest.methods) {
+		$.extend(_constructor.prototype, manifest.methods);
+	}
+	_constructor.prototype.templates = manifest.templates;
+	Echo.Utils.setNestedValue(window, manifest.name, _constructor);
+	return _constructor;
+};
+
+Echo.Control.skeleton = function(name) {
+	return {
+		"name": name,
+		"methods": {},
+		"renderers": {},
+		"templates": {},
+		"constructor": function(){ this.render(); }
+	};
+};
+
+// dynamic interface (available for class instances)
+
+// TODO: revisit function location and contract...
+Echo.Control.prototype.substitute = function(template, data) {
+	var self = this;
+	template = template.replace(Echo.Vars.regexps.matchSelf, function($0, $1) {
+		// FIXME: incorrect condition if 0, "", false or [] passed on purpose
+		return Echo.Utils.getNestedValue($1, self) ||
+			Echo.Utils.getNestedValue($1, self.data || {}) ||
+			self.config.get($1, "");
+	});
+	template = template.replace(Echo.Vars.regexps.matchLabel, function($0, $1) {
+		return self.labels.get($1, "");
+	});
+	template = template.replace(Echo.Vars.regexps.matchData, function($0, $1) {
+		return Echo.Utils.getNestedValue($1, data, "");
+	});
+	return template;
+};
+
+Echo.Control.prototype.render = function() {
+	// TODO: provide the ability to render 1 specific element
+	var control = this;
+	var target = $(this.config.get("target"));
+	// TODO: set specific CSS class...
+	var templates = {};
+	templates.raw = $.isFunction(this.template) ? this.template() : this.template;
+	templates.processed = $(this.substitute(templates.raw, this.data || {}));
+	templates.processed.find("*").andSelf().each(function(i, element) {
+		element = $(element);
+		var renderer = element.data("renderer");
+		if (renderer && control.renderers[renderer]) {
+			control.renderers[renderer][0].apply(control, [element]);
+		}
+	});
+	target.empty().append(templates.processed);
+};
+
+Echo.Control.prototype.rerender = function() {};
+
+Echo.Control.prototype.template = function() {
+	return this.templates.main;
+};
+
+Echo.Control.prototype.init = function(subsystems) {
+	var control = this;
+	$.map(subsystems, function(arg) {
+		var name = $.isArray(arg) ? arg.shift() : arg;
+		var args = $.isArray(arg) ? arg.shift() : [];
+		var result = control.init[name].apply(control, args);
+		if (typeof result != "undefined") {
+			control[name] = result;
+		}
+	});
+};
+
+Echo.Control.prototype.init.vars = function() {
+	return {"cache": {}};
+};
+
+Echo.Control.prototype.init.config = function(config) {
+	// TODO: copy init config function from Echo.Application
+	// TODO: need to assign proper events context for the instance via getUniqueString!
+	//       get parent context and add extra value!
+	//this.config.set("context", Echo.Utils.getUniqueString());
+	return new Echo.Configuration(config, this.manifest.config);
+};
+
+Echo.Control.prototype.init.events = function() {
+	var control = this;
+	var events = {
+		"prepare": function(params) {
+			params.context = control.config.get("context");
+			params.callback = $.proxy(params.callback, control);
+			return params;
+		},
+		"publish": function(params) {
+			Echo.Events.publish(events.prepare(params));
+		},
+		"subscribe": function(params) {
+			Echo.Events.subscribe(events.prepare(params));
+		},
+		"unsubscribe": Echo.Events.unsubscribe
+	};
+	$.each(control.manifest.events, function(topic, callback) {
+		events.subscribe({
+			"topic": topic,
+			"handler": $.proxy(callback, control)
+		});
+	});
+	return events;
+};
+
+Echo.Control.prototype.init.labels = function() {
+	return new Echo.Labels(this.manifest.labels, this.name);
+};
+
+Echo.Control.prototype.init.css = function() {
+	if (!this.manifest.css) return;
+	Echo.Utils.addCss(this.manifest.css, this.manifest.name);
+};
+
+Echo.Control.prototype.init.renderers = function() {
+	return Echo.Utils.foldl({}, this.manifest.renderers, function(renderer, acc, name) {
+		acc[name] = [renderer];
+	});
+};
+
+Echo.Control.prototype.init.user = function(callback) {
+	var control = this;
+	return Echo.UserSession({
+		"appkey": this.config.get("appkey"),
+		"ready": $.proxy(callback, control)
+	});
+};
+
+})(jQuery);
---
+++ src/identity-server/controls/auth.js
@@ -0,0 +1,176 @@
+(function($) {
+
+"use strict";
+
+if (Echo.Utils.isComponentDefined("Echo.IdentityServer.Controls.Auth")) return;
+
+var auth = Echo.Control.skeleton("Echo.IdentityServer.Controls.Auth");
+
+auth.config = {
+	"param": "test value"
+};
+
+auth.labels = {
+	"edit": "Edit",
+	"login": "Login",
+	"logout": "Logout",
+	"loggingOut": "Logging out...",
+	"or": "or",
+	"signup": "signup"
+};
+
+auth.events = {
+	"internal.User.onInvalidate": function() {
+		// TODO: pass control ref as "this"
+		$.fancybox.close();
+		this.rerender();
+	}
+};
+
+auth.templates.anonymous =
+	'<div class="echo-auth-anonymous echo-primaryFont">' +
+		'<span class="echo-auth-login echo-linkColor echo-clickable" data-renderer="login">' +
+			"{Label:login}" +
+		'</span>' +
+		'<span class="echo-auth-or echo-secondaryColor"> {Label:or} </span>' +
+		'<span class="echo-auth-signup echo-linkColor echo-clickable" data-renderer="signup">' +
+			"{Label:signup}" +
+		'</span>' +
+	'</div>';
+
+auth.templates.logged =
+	'<div class="echo-auth-logged echo-primaryFont echo-primaryColor">' +
+		'<div class="echo-auth-avatar" data-renderer="avatar"></div>' +
+		'<div class="echo-auth-name"></div>' +
+		'<div class="echo-auth-edit echo-linkColor echo-clickable">' +
+			"{Label:edit}" +
+		'</div>' +
+		'<div class="echo-auth-logout echo-linkColor echo-clickable">' +
+			"{Label:logout}" +
+		'</div>' +
+		'<div class="echo-clear"></div>' +
+	'</div>';
+
+auth.renderers.logout = function(element) { 
+	var self = this;
+	return element.click(function() {
+		element.empty().append(self.label("loggingOut"));
+		self.user.logout();
+	});
+};
+
+auth.renderers.login = function(element) {
+	return this.assembleIdentityControl("login", element);
+};
+
+auth.renderers.edit = function(element) {
+	return this.assembleIdentityControl("edit", element);
+};
+
+auth.renderers.signup = function(element) {
+	return this.assembleIdentityControl("signup", element);
+};
+
+auth.renderers.or = function(element) {
+	if (!this.config.get("identityManager.login") ||
+		!this.config.get("identityManager.signup") ||
+		!this.user.get("sessionID")) {
+			element.hide();
+	}
+	return element;
+};
+
+auth.renderers.avatar = function(element) {
+	var self = this;
+	var url = this.user.get("avatar", this.user.config.get("defaultAvatar"));
+	return element.append(
+		$("<img>", { "src": url }).bind({
+			"error" : function(){
+				$(this).attr("src", self.user.get("defaultAvatar"));
+			}
+		})
+	);
+};
+
+auth.renderers.name = function(element) {
+	return element.append(this.user.get("name", ""));
+};
+
+auth.methods.template = function() {
+	return this.templates[this.user.is("logged") ? "logged" : "anonymous"];
+};
+
+auth.methods.assembleIdentityControl = function(type, element) {
+	var self = this;
+	var data = this.config.get("identityManager." + type);
+	if (!data || !Backplane.getChannelID()) return element.hide();
+
+	var appendSessionID = function(url) {
+		var id = encodeURIComponent(Backplane.getChannelID());
+		var parts = Echo.Utils.parseUrl(url);
+		var session = parts["query"]
+			? parts["query"].match(/=$/) ? id : "&sessionID=" + id
+			: "sessionID=" + id;
+		return self.substitute("{Data:scheme}://{Data:domain}{Data:path}?{Data:query}{Data:fragment}", {
+			"scheme": parts["scheme"] || "http",
+			"domain": parts["domain"],
+			"path": parts["path"],
+			"query": (parts["query"] || "") + session,
+			"fragment": parts["fragment"] ? ("#" + parts["fragment"]) : ""
+		});
+	};
+	if (data.type == "script") {
+		return element.click(function() {
+			$.getScript(appendSessionID(data.url));
+		});
+	} else {
+		return element.fancybox({
+			"autoScale": false,
+			"height": data.height,
+			"href": appendSessionID(data.url),
+			"onClosed": function() {
+				// remove dynamic height/width calculations for overlay
+				if ($.browser.msie && document.compatMode != "CSS1Compat") {
+					var style = $("#fancybox-overlay").get(0).style;
+					style.removeExpression("height");
+					style.removeExpression("width");
+				}
+			},
+			"onComplete": function() {
+				// set fixed dimensions of the frame (for IE in quirks mode it should be smaller)
+				var delta = ($.browser.msie && document.compatMode != "CSS1Compat" ? 40 : 0);
+				$("#fancybox-frame").css({
+					"width": data.width - delta,
+					"height": data.height - delta
+				});
+			},
+			"onStart": function() {
+				Backplane.expectMessages("identity/ack");
+				// dynamical calculation of overlay height/width
+				if ($.browser.msie && document.compatMode != "CSS1Compat") {
+					var style = $("#fancybox-overlay").get(0).style;
+					style.setExpression("height", "Math.max(document.body.clientHeight, document.body.scrollHeight)");
+					style.setExpression("width", "Math.max(document.body.clientWidth, document.body.scrollWidth)");
+				}
+			},
+			"padding": 0,
+			"scrolling": "no",
+			"transitionIn": "elastic",
+			"transitionOut": "elastic",
+			"type": "iframe",
+			"width": data.width
+		});
+	}
+};
+
+auth.css =
+	".echo-auth-logout { float: right; }" +
+	".echo-auth-anonymous { text-align: right; }" +
+	".echo-auth-avatar { float: left; }" +
+	".echo-auth-avatar img { width: 24px; height: 24px; }" +
+	".echo-auth-name { float: left; font-size: 18px; line-height: 24px; margin-left: 5px; font-weight: bold; }" +
+	".echo-auth-edit { float: left; margin: 6px 0px 0px 12px; }";
+
+Echo.Control.create(auth);
+
+})(jQuery);
