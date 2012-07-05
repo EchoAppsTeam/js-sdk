024e998b9f811f020d274f0b03df5919437fa0af
Maksim Terekhin <neurostep@aboutecho.com>
2012-07-05 17:16:06 +0400

    fixed bug with submit params

--- src/stream-server/api.js
+++ src/stream-server/api.js
@@ -49,7 +49,7 @@ Echo.StreamServer.API.Request.prototype._submit = function() {
 	this.transport.config.set("url", this.config.get("submissionProxyURL"));
 	this.request(
 		$.extend(this.config.get("data"), {
-			"content": this._AS2KVL(this.config.get("data.content"))
+			"content": Echo.Utils.object2JSON(this._AS2KVL(this.config.get("data.content")))
 		})
 	);
 };
