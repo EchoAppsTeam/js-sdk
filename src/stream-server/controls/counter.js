573d16c427cdf721e333e0e87aa90c7dd3e9410f
Andrew Kushnir <kushnir.andrew@gmail.com>
2012-07-12 18:42:04 +0400

    Counter updated according to the upcoming Echo.API lib changes

--- src/stream-server/controls/counter.js
+++ src/stream-server/controls/counter.js
@@ -5,89 +5,90 @@ if (Echo.Utils.isComponentDefined("Echo.StreamServer.Controls.Counter")) return;
 var counter = Echo.Control.skeleton("Echo.StreamServer.Controls.Counter");
 
 counter.config = {
-	"liveUpdatesTimeout": 10 
+	"liveUpdatesTimeout": 10,
+	"infoMessages": {"layout": "compact"}
 };
 
-counter.events["Submit.onPostComplete"] =
-counter.events["Submit.onEditComplete"] = {
-	"context" : "global",
-	"handler" : function() {
-		this.APIRequest.send({"force": true});
-	}
-};
+$.map(["onPostComplete", "Plugins.Edit.onEditComplete"], function(name) {
+	counter.events["Echo.StreamServer.Controls.Submit." + name] = {
+		"context" : "global",
+		"handler" : function() {
+			this.get("request").send({"force": true});
+		}
+	};
+});
 
-counter.templates.main = '<span>{data:count}</span>';
+counter.templates.main = "<span>{data:count}</span>";
 
 counter.constructor = function() {
-	var self = this;
-	//TODO showmessage should be refactored
-	this.showMessage({"type": "loading"});
 	// data can be defined explicitly
 	// in this case we do not make API requests
-	if ($.isEmptyObject(this.data)){
-		this.request();
+	// TODO: no live updates for now if data is passed as a config value
+	if ($.isEmptyObject(this.get("data"))) {
+		this._request();
 	} else {
 		this.render();
-		this.listen();
 	}
 };
 
-counter.methods.request = function() {
-	var self = this;
-	Echo.StreamServer.API.request({
-		"endpoint": "count",
-		"method": "GET",
-		"data": {
-			"q": self.config.get("query"),
-			"appkey": self.config.get("appkey")
-		},
-		"onError": function(response) {
-			//TODO add error handling
-			// including more_than error
-			self.error(response);
-		},
-		"onData": function(response) {
-			//self.data = response;
-			self.update(response);
-			self.listen();
-		}
-	}).send();
+counter.methods.refresh = function() {
+	this.showMessage({"type": "loading"});
+	this.set("data", {});
+	this._request();
+	var component = Echo.Utils.getComponent("Echo.StreamServer.Controls.Counter");
+	component.parent.refresh.call(this);
 };
 
-counter.methods.listen = function() {
-	var self = this;
-	this.APIRequest = Echo.StreamServer.API.request({
-		"endpoint": "count",
-		"liveUpdatesTimeout": self.config.get("liveUpdatesTimeout"),
-		"method": "GET",
-		"recurring": true,
-		"data": {
-			"q": self.config.get("query"),
-			"appkey": self.config.get("appkey")
-		},
-		"onError": function(response) {
-			//TODO add error handling
-			// including more_than error
-			console.log("error");
-			self.error(response);
-			self.APIRequest.abort();
-		},
-		"onData": function(response) {
-			//TODO check if counter was updated
-			self.data = response;
-			self.update(response);
-		}
+// internal functions
+
+/* TODO: enable this function instead of the current one when:
+         - Echo.API supports first and next requests in a single instance
+         - when Transport will be able to handle config updates
+counter.methods._request = function() {
+	var request = this.get("request");
+	if (!request) {
+		request = Echo.StreamServer.API.request({
+			"endpoint": "count",
+			"method": "GET",
+			"liveUpdatesTimeout": this.config.get("liveUpdatesTimeout"),
+			"recurring": true,
+			"onError": $.proxy(this._error, this),
+			"onData": $.proxy(this._update, this)
+		});
+		this.set("requst", request);
+	}
+	request.config.set("data", {
+		"q": this.config.get("query"),
+		"appkey": this.config.get("appkey")
 	});
-	this.APIRequest.send();
+	request.send();
+};
+*/
+
+counter.methods._request = function() {
+	var request = this.get("request");
+	if (!request) {
+		request = Echo.StreamServer.API.request({
+			"endpoint": "count",
+			"method": "GET",
+			"data": {
+				"q": this.config.get("query"),
+				"appkey": this.config.get("appkey")
+			},
+			"onError": $.proxy(this._error, this),
+			"onData": $.proxy(this._update, this)
+		});
+		this.set("requst", request);
+	}
+	request.send();
 };
 
-counter.methods.update = function(data) {
+counter.methods._update = function(data) {
 	if ($.isEmptyObject(this.data) || this.data.count != data.count) {
-		this.data = data;
+		this.set("data", data);
 		this.render();
-		console.log("Counter.onUpdate");
 		this.events.publish({
-			"topic": "Counter.onUpdate",
+			"topic": "onUpdate",
 			"data": {
 				"data": data,
 				"query": this.config.get("query"),
@@ -97,36 +98,13 @@ counter.methods.update = function(data) {
 	}
 };
 
-counter.methods.error = function(data) {
+counter.methods._error = function(data) {
 	if (data.errorCode === "more_than") {
-		this.data.count = data.errorMessage + "+";
-		this.rerender();
+		this.set("data.count", data.errorMessage + "+");
+		this.render();
 	} else {
-		//TODO update showMessage function
-		this.showMessage({"type": "error"});
+		this.showMessage({"type": "error", "data": data});
 	}
-};
-
-/*
- target.html(data.errorCode == "more_than" ? (data.errorMessage + "+") : data.count);
-        if ($.isEmptyObject(this.data) || this.data.count != data.count) {
-                this.publish("Counter.onUpdate", {
-                        "data": data,
-                        "query": this.config.get("query", ""),
-                        "target": this.config.get("target").get(0)
-                });
-        }
-*/
-
-counter.methods.refresh = function() {
-	//TODO we should consider moving refresh logic to the parent method
-	this.APIRequest.abort();
-	this.data = {};
-	//TODO showmessage should be refactored
-	this.showMessage({"type": "loading"});
-	this.request();
-	var component = Echo.Utils.getComponent("Echo.StreamServer.Controls.Counter");
-	component.parent.refresh.call(this);
 };
 
 Echo.Control.create(counter);
