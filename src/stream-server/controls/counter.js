4eb150d38a23b6c8940c91e63a378cb82a1c271c
Leonid Mosenkov <leon@aboutecho.com>
2012-07-17 14:32:26 +0400

    further counter inline docs updates

--- src/stream-server/controls/counter.js
+++ src/stream-server/controls/counter.js
@@ -31,10 +31,29 @@ counter.templates.main = "<span>{data:count}</span>";
 * @param {String} config.target Specifies the DOM element where the control will be displayed.
 * @param {String} config.appkey Specifies the customer application key. You can use the "test.echoenabled.com" appkey for testing purposes.
 * @param {String} config.query Specifies the search query to generate the necessary data set. It must be constructed according to the <a href="http://wiki.aboutecho.com/w/page/23491639/API-method-search" target="_blank">"search" API</a> method specification.
+*
+*     new Echo.StreamServer.Controls.Counter({
+*         "target": document.getElementById("container"),
+*         "appkey": "test.aboutecho.com",
+*         "query" : "childrenof:http://example.com/test/*"
+*     });
+* @param {Object} config.data Specifies predefined items count which should be displayed by the application.
+*     new Echo.Counter({
+*         ...
+*         "data": {"count": 100},
+*         ...
+*     });
 * @param {Number} [config.liveUpdatesTimeout=10] Specifies the timeout between the live updates requests (in seconds).
 * @param {Object} [config.infoMessages] Customizes the look and feel of info messages, for example "loading" and "error".
 * @param {String} [config.infoMessages.layout="compact"] Specifies the layout of the info message. By default can be set to "compact" or "full".
 * @param {Boolean} [config.infoMessages.enabled=true] Specifies if info messages should be rendered.
+*     new Echo.StreamServer.Controls.Counter({
+*         ...
+*         "infoMessages" : {
+*             "enabled" : true,
+*             "layout" : "full"
+*         }
+*     });
 * @return {Object} Returns the reference to the corresponding Echo.StreamServer.Controls.Counter instance.
 */
 
