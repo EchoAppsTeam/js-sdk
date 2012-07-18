1cdd3525536568cae1816e04dbcd4e2827797416
Leonid Mosenkov <leon@aboutecho.com>
2012-07-18 17:57:37 +0400

    auth test fixed

--- tests/unit/identity-server/controls/auth.js
+++ tests/unit/identity-server/controls/auth.js
@@ -9,38 +9,6 @@ suite.prototype.info = {
 
 suite.prototype.tests = {};
 
-suite.prototype.tests.anonymousUser = {
-	"config" : {
-		"async" : true,
-		"testTimeout" : 20000, // 20 secs
-		"user": {"status": "anonymous"}
-	},
-	"check" : function() {
-		var identityManager = {"width": 400, "height": 240, "url": "https://echo.rpxnow.com/openid/embed?flags=stay_in_window,no_immediate&token_url=http%3A%2F%2Fjs-kit.com%2Fapps%2Fjanrain%2Fwaiting.html&bp_channel="};
-		var target = document.getElementById("qunit-fixture");	
-		$(target).empty();
-		var auth = new Echo.IdentityServer.Controls.Auth({
-			"target": target,
-			"appkey": "test.aboutecho.com",
-			"identityManager": {
-				"login": identityManager,
-				"signup": identityManager
-			}
-		});
-		var handlerId = auth.events.subscribe({
-			"topic"   : "Echo.IdentityServer.Controls.Auth.onRender",
-			"handler" : function(topic, params) {
-				// unsubscribe to avoid multiple test cases execution
-				auth.events.unsubscribe({
-					"handlerId" : handlerId
-				});
-				QUnit.ok($(target).html().match(/echo-identityserver-controls-auth-userAnonymous/),
-					'Checking the anonymous mode rendering');
-				QUnit.start();
-			}
-		});
-	}
-};
 
 suite.prototype.tests.loggedInUser = {
 	"config" : {
@@ -52,19 +20,13 @@ suite.prototype.tests.loggedInUser = {
 		var identityManager = {"width": 400, "height": 240, "url": "https://echo.rpxnow.com/openid/embed?flags=stay_in_window,no_immediate&token_url=http%3A%2F%2Fjs-kit.com%2Fapps%2Fjanrain%2Fwaiting.html&bp_channel="};
 		var target = document.getElementById("qunit-fixture");	
 		$(target).empty();
-		var auth = new Echo.IdentityServer.Controls.Auth({
-			"target": target,
-			"appkey": "test.aboutecho.com",
-			"identityManager": {
-				"login": identityManager,
-				"signup": identityManager
-			}
-		});
-		var handlerId = auth.events.subscribe({
+
+		var handlerId = Echo.Events.subscribe({
 			"topic"   : "Echo.IdentityServer.Controls.Auth.onRender",
+			"context" : "global",
 			"handler" : function(topic, params) {
 				// unsubscribe to avoid multiple test cases execution
-				auth.events.unsubscribe({
+				Echo.Events.unsubscribe({
 					"handlerId" : handlerId
 				});
 				QUnit.ok($(target).html().match(/echo-identityserver-controls-auth-userLogged/),
@@ -72,6 +34,48 @@ suite.prototype.tests.loggedInUser = {
 				QUnit.start();
 			}
 		});
+		new Echo.IdentityServer.Controls.Auth({
+			"target": target,
+			"appkey": "test.aboutecho.com",
+			"identityManager": {
+				"login": identityManager,
+				"signup": identityManager
+			}
+		});
 	}
 };
+
+suite.prototype.tests.anonymousUser = {
+	"config" : {
+		"async" : true,
+		"testTimeout" : 20000, // 20 secs
+		"user": {"status": "anonymous"}
+	},
+	"check" : function() {
+		var identityManager = {"width": 400, "height": 240, "url": "https://echo.rpxnow.com/openid/embed?flags=stay_in_window,no_immediate&token_url=http%3A%2F%2Fjs-kit.com%2Fapps%2Fjanrain%2Fwaiting.html&bp_channel="};
+		var target = document.getElementById("qunit-fixture");	
+		$(target).empty();
+		var handlerId = Echo.Events.subscribe({
+			"topic"   : "Echo.IdentityServer.Controls.Auth.onRender",
+			"handler" : function(topic, params) {
+				// unsubscribe to avoid multiple test cases execution
+				Echo.Events.unsubscribe({
+					"handlerId" : handlerId
+				});
+				QUnit.ok($(target).html().match(/echo-identityserver-controls-auth-userAnonymous/),
+					'Checking the anonymous mode rendering');
+				QUnit.start();
+			}
+		});
+		new Echo.IdentityServer.Controls.Auth({
+			"target": target,
+			"appkey": "test.aboutecho.com",
+			"identityManager": {
+				"login": identityManager,
+				"signup": identityManager
+			}
+		});
+	}
+};
+
 })(jQuery);
