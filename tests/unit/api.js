394054dc4033aa3dddc3821f53412efd69c0fc7b
Leonid Mosenkov <leon@aboutecho.com>
2012-07-18 18:08:49 +0400

    fixed api.js test

--- tests/unit/api.js
+++ tests/unit/api.js
@@ -191,8 +191,8 @@ suites.StreamServerAPI.prototype.tests.PrivateFunctionsTests = {
 		QUnit.ok(req._isWaitingForData({"result": "error", "errorCode": "view_limit", "extra": {}}), "Checking if error responsed JSON contains waiting error code");
 		QUnit.ok(req._isErrorWithTimer({"result": "error", "errorCode": "view_update_capacity_exceeded", "extra": {}}), "Checking if error responsed JSON contains error timer code");
 		QUnit.deepEqual(req._AS2KVL(this.items.post), {
-			"content": "For the record, I think your neck looked just fine.\n\nPeace out, Nora.",
 			"avatar": "http://my.nymag.com/thenext_mrsbass/picture?type=square",
+			"content": "For the record, I think your neck looked just fine.\n\nPeace out, Nora.",
 			"markers": undefined,
 			"name": "TheNext_MrsBass",
 			"source": {
@@ -203,11 +203,11 @@ suites.StreamServerAPI.prototype.tests.PrivateFunctionsTests = {
 			"target": "http://nymag.com/daily/intel/2012/06/nora-ephron-1941-2012.html",
 			"verb": "post",
 			"type": "comment",
-			"itemURIPattern": ""
+			"itemURIPattern": undefined
 		}, "Check decompiler from AS to KVL");
 		QUnit.deepEqual(req._AS2KVL(this.items.postWithMetadata), {
-			"content": "For the record, I think your neck looked just fine.\n\nPeace out, Nora.",
 			"avatar": "http://my.nymag.com/thenext_mrsbass/picture?type=square",
+			"content": "For the record, I think your neck looked just fine.\n\nPeace out, Nora.",
 			"markers": "marker1,marker2,marker3",
 			"name": "TheNext_MrsBass",
 			"source": {
@@ -218,32 +218,19 @@ suites.StreamServerAPI.prototype.tests.PrivateFunctionsTests = {
 			"target": "http://nymag.com/daily/intel/2012/06/nora-ephron-1941-2012.html",
 			"verb": "post",
 			"type": "comment",
-			"itemURIPattern": ""
+			"itemURIPattern": undefined
 		}, "Check decompiler from AS to KVL with post and metadata");
 		QUnit.deepEqual(req._AS2KVL(this.items.postWithMetadata.slice(1)), [
-			{
-				"content": "tag1,tag2,tag3",
-				"avatar": "http://my.nymag.com/thenext_mrsbass/picture?type=square",
-				"markers": "marker1,marker2,marker3",
-				"name": "TheNext_MrsBass",
-				"source": undefined,
-				"tags": "tag1,tag2,tag3",
-				"target": "http://nymag.com/daily/intel/2012/06/nora-ephron-1941-2012.html",
-				"verb": "tag",
-				"type": "tag",
-				"itemURIPattern": ""
-			}, {
-				"content": "marker1,marker2,marker3",
-				"avatar": "http://my.nymag.com/thenext_mrsbass/picture?type=square",
-				"markers": "marker1,marker2,marker3",
-				"name": "TheNext_MrsBass",
-				"source": undefined,
-				"tags": "tag1,tag2,tag3",
-				"target": "http://nymag.com/daily/intel/2012/06/nora-ephron-1941-2012.html",
-				"verb": "tag",
-				"type": "marker",
-				"itemURIPattern": ""
-			}
+		{
+			"tags": "tag1,tag2,tag3",
+			"verb": "tag",
+			"target": "http://nymag.com/daily/intel/2012/06/nora-ephron-1941-2012.html"
+		},
+		{
+			"tags": "marker1,marker2,marker3",
+			"verb": "tag",
+			"target": "http://nymag.com/daily/intel/2012/06/nora-ephron-1941-2012.html"
+		}
 		], "Check decompiler from AS to KVL with metadata only");
 		req._changeLiveUpdatesTimeout({
 			"liveUpdatesTimeout": 4
