a518159046e66b15b7f542cfb38b1a650e450177
Andrew Kushnir <kushnir.andrew@gmail.com>
2012-07-28 00:24:20 +0400

    Reply and Edit plugin tests updated

--- tests/unit/stream-server/plugins/edit.js
+++ tests/unit/stream-server/plugins/edit.js
@@ -1,6 +1,8 @@
 (function($) {
 
-var suite = Echo.Tests.Unit.PluginsEdit = function() {};
+var suite = Echo.Tests.Unit.PluginsEdit = function() {
+	this.constructPluginRenderersTest();
+};
 
 suite.prototype.tests = {};
 
@@ -8,33 +10,6 @@ suite.prototype.info = {
 	"className": "Echo.StreamServer.Controls.Submit.Plugins.Edit",
 	"suiteName": "Edit plugin",
 	"functions": []
-};
-
-suite.prototype.tests.renderersTest = {
-	"config": {
-		"async": true,
-		"testTimeout": 10000
-	},
-	"check": function() {
-		var test = this;
-		var target = document.getElementById("qunit-fixture");
-		var identityManager = {
-			"width": 400,
-			"height": 240,
-			"url": "https://echo.rpxnow.com/openid/embed?flags=stay_in_window,no_immediate&token_url=http%3A%2F%2Fjs-kit.com%2Fapps%2Fjanrain%2Fwaiting.html&bp_channel="
-		};
-		var submit = new Echo.StreamServer.Controls.Submit({
-			"target": $(target).empty(),
-			"appkey": this.config.appkey,
-			"plugins": [{
-				"name": "Edit"
-			}],
-			"ready": function() {
-				test.executePluginRenderersTest(this.getPlugin("Edit"));
-				QUnit.start();
-			}
-		});
-	}
 };
 
 })(jQuery);
--- tests/unit/stream-server/plugins/reply.js
+++ tests/unit/stream-server/plugins/reply.js
@@ -1,6 +1,8 @@
 (function($) {
 
-var suite = Echo.Tests.Unit.PluginsReply = function() {};
+var suite = Echo.Tests.Unit.PluginsReply = function() {
+	this.constructPluginRenderersTest();
+};
 
 suite.prototype.tests = {};
 
@@ -8,33 +10,6 @@ suite.prototype.info = {
 	"className": "Echo.StreamServer.Controls.Stream.Item.Plugins.Reply",
 	"suiteName": "Reply plugin",
 	"functions": []
-};
-
-suite.prototype.tests.renderersTest = {
-	"config": {
-		"async": true,
-		"testTimeout": 10000
-	},
-	"check": function() {
-		var test = this;
-		var target = document.getElementById("qunit-fixture");
-		$(target).empty();
-		var stream = new Echo.StreamServer.Controls.Stream({
-			"target": target,
-			"query": "childrenof:" + this.config.dataBaseLocation,
-			"appkey": this.config.appkey,
-			"plugins": [{
-				"name": "Reply"
-			}],
-			"ready": function() {
-				var item = this.threads[0];
-				if (item) {
-					test.executePluginRenderersTest(item.getPlugin("Reply"));
-					QUnit.start();
-				}
-			}
-		});
-	}
 };
 
 })(jQuery);
