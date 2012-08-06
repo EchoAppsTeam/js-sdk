3451325deece7a4f1bd68451b1e0829d564eec4f
Nikolay Safin <safin@aboutecho.com>
2012-08-06 10:08:14 +0400

    Added list of contracts

---
+++ tests/helpers/events.js
@@ -0,0 +1,260 @@
+Echo.Tests.Events = Echo.Tests.Events || {};
+
+Echo.Tests.Events.contracts = {
+	"Echo.StreamServer.Controls.Stream.onDataReceive": {
+		"entries": [],
+		"initial": true,
+		"query": "",
+		"target": $("<div>")
+	},
+	"Echo.StreamServer.Controls.Stream.onRender": {
+		"query": "",
+		"target": $("<div>")
+	},
+	"Echo.StreamServer.Controls.Stream.onRerender": {
+		"query": "",
+		"target": $("<div>")
+	},
+	"Echo.StreamServer.Controls.Stream.onMoreButtonPress": {
+		"query": "",
+		"target": $("<div>")
+	},
+	"Echo.StreamServer.Controls.Stream.Item.onReceive": {
+		"query": "",
+		"target": $("<div>"),
+		"item": {
+			"target": $("<div>"),
+			"data": {
+				"entries": [],
+				"id": "",
+				"updated": ""
+			}
+		}
+	},
+	"Echo.StreamServer.Controls.Stream.Item.onRender": {
+		"query": "",
+		"target": $("<div>"),
+		"item": {
+			"target": $("<div>"),
+			"data": {
+				"entries": [],
+				"id": "",
+				"updated": ""
+			}
+		}
+	},
+	"Echo.StreamServer.Controls.Stream.Item.onRerender": {
+		"query": "",
+		"target": $("<div>"),
+		"item": {
+			"target": $("<div>"),
+			"data": {
+				"entries": [],
+				"id": "",
+				"updated": ""
+			}
+		}
+	},
+	"Echo.UserSession.onInit": {
+		"echo": {
+			"roles": [],
+				"state": "",
+				"markers": []
+		},
+		"poco": {
+			"entry": {
+				"accounts": []
+			},
+			"startIndex": 0,
+				"itemsPerPage": 0,
+				"totalResults": 0
+		}
+	},
+	"Echo.UserSession.onInvalidate": {
+		"echo": {
+			"roles": [],
+			"state": "",
+			"markers": []
+		},
+		"poco": {
+			"entry": {
+				"accounts": []
+			},
+			"startIndex": 0,
+			"itemsPerPage": 0,
+			"totalResults": 0
+		}
+	},
+	"Echo.IdentityServer.Controls.Auth.onRender": {},
+	"Echo.IdentityServer.Controls.Auth.onReady": {},
+	"Echo.StreamServer.Controls.Stream.Item.Plugins.Curation.onSelect": {
+		"query": "",
+		"target": $("<div>"),
+		"item": {
+			"target": $("<div>"),
+			"data": {
+				"entries": [],
+				"id": "",
+				"updated": ""
+			}
+		}
+	},
+	"Echo.StreamServer.Controls.Stream.Item.Plugins.Curation.onUnselect": {
+		"query": "",
+		"target": $("<div>"),
+		"item": {
+			"target": $("<div>"),
+			"data": {
+				"entries": [],
+				"id": "",
+				"updated": ""
+			}
+		}
+	},
+	"Echo.StreamServer.Controls.Stream.Plugins.Like.onLikeComplete": {
+		"query": "",
+		"target": $("<div>"),
+		"item": {
+			"target": $("<div>"),
+			"data": {
+				"entries": [],
+				"id": "",
+				"updated": ""
+			}
+		}
+	},
+	"Echo.StreamServer.Controls.Stream.Plugins.Like.onUnlikeComplete": {
+		"query": "",
+		"target": $("<div>"),
+		"item": {
+			"target": $("<div>"),
+			"data": {
+				"entries": [],
+				"id": "",
+				"updated": ""
+			}
+		}
+	},
+	"Echo.StreamServer.Controls.Stream.Plugins.CommunityFlag.onFlagComplete": {
+		"query": "",
+		"target": $("<div>"),
+		"item": {
+			"target": $("<div>"),
+			"data": {
+				"entries": [],
+				"id": "",
+				"updated": ""
+			}
+		}
+	},
+	"Echo.StreamServer.Controls.Stream.Plugins.CommunityFlag.onUnflagComplete": {
+		"query": "",
+		"target": $("<div>"),
+		"item": {
+			"target": $("<div>"),
+			"data": {
+				"entries": [],
+				"id": "",
+				"updated": ""
+			}
+		}
+	},
+	"Echo.StreamServer.Controls.Submit.onRender": {
+		"target": $("<div>"),
+		"targetURL": "",
+		"data": {
+			"entries": [],
+			"id": "",
+			"updated": ""
+		}
+	},
+	"Echo.StreamServer.Controls.Submit.onRerender": {
+		"target": $("<div>"),
+		"targetURL": "",
+		"data": {
+			"entries": [],
+			"id": "",
+			"updated": ""
+		}
+	},
+	"Echo.StreamServer.Controls.Submit.onExpand": {
+		"target": $("<div>"),
+		"targetURL": "",
+		"data": {
+			"entries": [],
+			"id": "",
+			"updated": ""
+		}
+	},
+	"Echo.StreamServer.Controls.Submit.onCollapse": {
+		"target": $("<div>"),
+		"targetURL": "",
+		"data": {
+			"entries": [],
+			"id": "",
+			"updated": ""
+		}
+	},
+	"Echo.StreamServer.Controls.Submit.onPostInit": {
+		"target": $("<div>"),
+		"targetURL": "",
+		"data": {
+			"entries": [],
+			"id": "",
+			"updated": ""
+		},
+		"postData": {
+			"avatar": "",
+			"content": "",
+			"markers": "",
+			"name": "",
+			"tags": "",
+			"target": "",
+			"url": "",
+			"verb": "post"
+		}
+	},
+	"Echo.StreamServer.Controls.Submit.onPostComplete": {
+		"target": $("<div>"),
+		"targetURL": "",
+		"data": {
+			"entries": [],
+			"id": "",
+			"updated": ""
+		},
+		"postData": {
+			"avatar": "",
+			"content": "",
+			"markers": "",
+			"name": "",
+			"tags": "",
+			"target": "",
+			"url": "",
+			"verb": "post"
+		}
+	},
+	"Echo.StreamServer.Controls.Submit.onEditInit": {
+		"target": $("<div>"),
+		"targetURL": "",
+		"data": {
+			"entries": [],
+			"id": "",
+			"updated": ""
+		},
+		"postData": {
+			"verb": "update|mark|unmark|tag|untag",
+			"target": "",
+			"markers": "",
+			"tags": "",
+			"field": "",
+			"value": ""
+		}
+	},
+	"Echo.StreamServer.Controls.Counter.onUpdate": {
+		"target": $("<div>"),
+		"query": "",
+		"data": {
+			"count": 0
+		}
+	}
+};
