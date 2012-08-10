132feb015cd3d549f886ae21f59e6d636d9d526f
Nikolay Safin <safin@aboutecho.com>
2012-08-10 17:56:01 +0400

    Twitter intents plugin (draft in progress)

--- src/stream-server/plugins/twitter-intents.js
+++ src/stream-server/plugins/twitter-intents.js
@@ -1,4 +1,4 @@
-(function($) {
+(function() {
 
 /**
  * @class Echo.StreamServer.Controls.Stream.Plugins.TwitterIntents
@@ -20,55 +20,190 @@ if (Echo.Plugin.isDefined(plugin)) return;
 plugin.config = {
 };
 
-plugin.init = function() {
-	var self = this;
-	var item = this.component;
-	this.extendTemplate("insertBefore", "authorName", plugin.usernameTemplate);
-
-
-
-	//item.addButtonSpec("Moderation", self._assembleButton(capitalize(action)));
-	//this.events.subscribe({
-	//	"topic": "internal.Echo.StreamServer.Controls.Stream.Item.Plugin.Moderation.onUserUpdate",
-	//	"handler": function(topic, args) {
-	//		if (args.item.data.actor.id !== item.data.actor.id) return;
-	//		item.data.actor[args.data.field] = args.data.value;
-	//		item.render();
-	//		return {"stop": ["bubble"]};
-	//	}
-	//});
-};
-
-plugin.usernameTemplate = '<span class="{plugin.class}tweetUserName echo-linkColor"></span>';
-
 plugin.labels = {
-	"reply": "Reply",
+	"tweet": "Reply",
 	"retweet": "Retweet",
 	"favorite": "Favorite",
 	"comment": "Comment"
 };
 
-plugin.renderers.status = function(element) {
+//plugin.dependencies = [{
+//	"loaded": function() { return !!window.twttr; },
+//	"url": "http://platform.twitter.com/widgets.js"
+//}];
+
+plugin.init = function() {
+	var item = this.component;
+
+	if (!this.isTweet(item)) return false;
+	// TODO: improve this block
+	item.config.set("contentTransformations", {
+		"text": ["smileys", "urls", "newlines"],
+		"html": ["smileys", "urls", "newlines"],
+		"xhtml": ["smileys", "urls"]
+	});
+
+	item.config.set("plugins.Like.enabled", false);
+	item.config.set("plugins.Reply.enabled", false);
+
+	this.extendTemplate("insertBefore", "authorName", plugin.templates.usernameTemplate);
+	this.extendTemplate("insertBefore", "date", plugin.templates.twitterIcon);
+
+	item.addButtonSpec(this.name, this._assembleButton("tweet"));
+	item.addButtonSpec(this.name, this._assembleButton("retweet"));
+	item.addButtonSpec(this.name, this._assembleButton("favorite"));
+};
+
+plugin.events = {
+	"Echo.StreamServer.Controls.Stream.Item.onReady": function(topic, args) {
+		var activeClass = this.cssPrefix + "activeButton";
+		var plugin = this, item = this.component;
+		$.map(item.buttons[this.name], function(name) {
+			name.element.unbind("click").unbind("mouseover mouseout")
+				.hover(
+					function() { name.element.addClass(activeClass); },
+					function() { name.element.removeClass(activeClass); }
+				);
+		});
+	}
+};
+
+plugin.templates = {
+	"usernameTemplate": '<div class="{plugin.class:tweetUserName} echo-linkColor"></div>',
+	"twitterIcon": '<div class="{plugin.class:twitterIcon}"></div>'
+};
+
+plugin.component.renderers.authorName = function(element) {
+	var item = this.component;
+	return element.append(item.data.actor.title).removeClass("echo-linkColor").addClass(this.cssPrefix + "tweetScreenName echo-secondaryColor");
+};
+
+plugin.component.renderers._buttonsDelimiter = function(element) {
+	var item = this.component;
+	var result = item.parentRenderer("_buttonsDelimiter", arguments);
+	var posDelimiter = item.buttonSpecs[this.name].length;
+
+	element.find("span[class*='button-delim']").eq(posDelimiter).text(" | ");
+
+	return result;
+};
+
+plugin.component.renderers.via = function(element) {
+	var item = this.component;
+	var provider = Echo.Utils.hyperlink({
+		"href": item.data.provider.uri,
+		"caption": item.data.provider.name
+	}, {
+		"openInNewWindow": item.config.get("openLinksInNewWindow"),
+		"skipEscaping": true
+	});
+	element.html('&nbsp;' + item.labels.get("viaLabel") + '&nbsp;' + provider);
+	return item.parentRenderer("via", arguments);
+};
+
+plugin.component.renderers.from = function(element) {
+	var item = this.component;
+	var source = Echo.Utils.hyperlink({
+		"href": item.data.source.uri,
+		"caption": item.data.source.name
+	}, {
+		"openInNewWindow": item.config.get("openLinksInNewWindow"),
+		"skipEscaping": true
+	});
+	element.html('&nbsp;' + item.labels.get("fromLabel") + '&nbsp;' + source);
+	return item.parentRenderer("from", arguments);
+};
+
+plugin.component.renderers.date = function(element) {
+	var item = this.component;
+	item._calcAge();
+
+	var date = Echo.Utils.hyperlink({
+		"caption": item.age,
+		"href": item.data.object.id,
+		"class": this.cssPrefix + "date"
+	}, {
+		"openInNewWindow": item.config.get("openLinksInNewWindow"),
+		"skipEscaping": true
+	});
+
+	return element.html(date);
+};
+
+plugin.renderers.twitterIcon = function(element) {
+	var item = this.component;
+	var icon = Echo.Utils.hyperlink({
+		"caption": '<img src="'+ item.data.source.icon+ '"/>',
+		"href": item.data.source.uri,
+		"class": ""
+	}, {
+		"openInNewWindow": item.config.get("openLinksInNewWindow"),
+		"skipEscaping": true
+	});
+	return element.html(icon);
+};
+
+plugin.renderers.tweetUserName = function(element) {
+	var item = this.component;
+	return element.append(Echo.Utils.hyperlink({
+		"href": item.data.actor.id,
+		"caption": this.extractTwitterID(item)
+	}, {
+		"openInNewWindow": item.config.get("openLinksInNewWindow"),
+		"skipEscaping": true
+	}));
 };
 
 plugin.methods._assembleButton = function(name) {
+	var plugin = this, item = this.component;
+	var match = item.data.object.id.match(/\/(\d+)$/);
+	var id = match && match[1];
+	return function() {
+		return {
+			"name": name,
+			"label": plugin.labels.get(name),
+			"template": Echo.Utils.hyperlink({
+				"href": "https://twitter.com/intent/" + name + "?in_reply_to=" + id + "&tweet_id=" + id,
+				"class": "echo-clickable " + plugin.cssPrefix + "intentControl echo-secondaryColor",
+				"caption":
+					'<span class="' + plugin.cssPrefix + 'icon ' + plugin.cssPrefix + 'icon-{data:name}">&nbsp;</span>' +
+					'<span>{data:label}</span>'
+			}, {
+				"openInNewWindow": item.config.get("openLinksInNewWindow"),
+				"skipEscaping": true
+			}),
+			"visible": id && plugin.isTweet(item)
+		};
+	};
+};
+
+plugin.methods.isTweet = function(item) {
+	return item.data.source.name == "Twitter";
+};
+
+plugin.methods.extractTwitterID = function(item) {
+	var match = item.data.actor.id.match(/twitter.com\/(.*)/);
+	return match ? match[1] : item.data.actor.id;
 };
 
 plugin.css =
 	".{class:avatar} a img { border: 0px; }" +
 	".{plugin.class:userName} { float: left; font-size: 15px; font-weight: bold; }" +
 	".{plugin.css:screenName} { margin-left: 4px; font-size: 11px; font-weight: normal; padding-top: 1px; }" +
-	".{plugin.class:userName} a, .{plugin.class:intentControl} { text-decoration: none; }" +
-	".{plugin.class:permalink} { float: left; text-decoration: none; }" +
-	".{plugin.class:permalink} .{class:item-date} { float: none; }" +
+	".{plugin.class:userName} a, .{plugin.class:tweetUserName} a, .{plugin.class:intentControl} { text-decoration: none; }" +
 	".{plugin.class:icon} { width: 15px; height: 15px; display: inline-block; margin-right: 3px; background: url(https://si0.twimg.com/images/dev/cms/intents/icons/sprites/everything-spritev2.png) no-repeat; }" +
-	".{plugin.class:iconReply} { background-position: 0px -2px; }" +
-	".{plugin.class:iconRetweet} { background-position: -80px -2px; }" +
-	".{plugin.class:iconFavorite} { background-position: -32px -2px; }" +
-	".{plugin.class:activeButton} .{plugin.class:iconReply} { background-position: -16px -2px; }" +
-	".{plugin.class:activeButton} .{plugin.class:iconRetweet} { background-position: -96px -2px; }" +
-	".{plugin.class:activeButton} .{plugin.class:iconFavorite} { background-position: -48px -2px; }";
+	".{plugin.class:icon-tweet} { background-position: 0px -2px; }" +
+	".{plugin.class:icon-retweet} { background-position: -80px -2px; }" +
+	".{plugin.class:icon-favorite} { background-position: -32px -2px; }" +
+	".{plugin.class:activeButton} .{plugin.class:icon-tweet} { background-position: -16px -2px; }" +
+	".{plugin.class:activeButton} .{plugin.class:icon-retweet} { background-position: -96px -2px; }" +
+	".{plugin.class:activeButton} .{plugin.class:icon-favorite} { background-position: -48px -2px; }" +
+	".{plugin.class:tweetUserName} { float: left; font-size: 15px; font-weight: bold; }" +
+	".{plugin.class:twitterIcon} { float: left; margin-right: 3px; }" +
+	".{plugin.class:date} { text-decoration: none; color: #C6C6C6; }" +
+	".{plugin.class:date}:hover { color: #476CB8; }" +
+	".{plugin.class:tweetScreenName} { font-size: 11px; font-weight: normal; margin-left: 4px; padding-top: 1px; }";
 
 Echo.Plugin.create(plugin);
 
-})(jQuery);
+})();
---
+++ tests/unit/stream-server/plugins/twitter-intents.js
@@ -0,0 +1,15 @@
+(function($) {
+
+	var suite = Echo.Tests.Unit.PluginsTwitterIntents = function() {
+		this.constructPluginRenderersTest();
+	};
+
+	suite.prototype.tests = {};
+
+	suite.prototype.info = {
+		"className": "Echo.StreamServer.Controls.Stream.Item.Plugins.TwitterIntents",
+		"suiteName": "TwitterIntents plugin",
+		"functions": []
+	};
+
+})(Echo.jQuery);
