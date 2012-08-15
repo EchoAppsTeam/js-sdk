23848d514e3675272018ddeef7eca273919799e5
Andrew Kushnir <kushnir.andrew@gmail.com>
2012-08-14 21:33:28 -0400

    Cosmetic update of the Echo jQuery compatibility test

--- tests/unit/noconflict.js
+++ tests/unit/noconflict.js
@@ -2,29 +2,32 @@
 
 	var suite = Echo.Tests.Unit.NoConflict = function() {};
 
-	suite.prototype.info = {};
+	suite.prototype.info = {
+		"suiteName": "Echo jQuery"
+	};
 
 	suite.prototype.tests = {};
 
-	suite.prototype.tests.jqueryVersion = {
+	suite.prototype.tests.compatibilityTest = {
 		"config": {
-		"async": true,
-		"testTimeout": 20000 // 20 secs
+			"async": true,
+			"testTimeout": 20000 // 20 secs
 		},
 		"check": function() {
 			var testVersion = "1.4.1";
 
-			QUnit.ok(!(window.$ || window.jQuery), "jQuery not defined in window namespace");
+			QUnit.ok(!(window.$ || window.jQuery),
+				"Checking if jQuery is not defined in the window namespace");
 
 			Echo.Loader.download({
 				"scripts" : [{"url": "https://ajax.googleapis.com/ajax/libs/jquery/" + testVersion + "/jquery.min.js"}],
 				"callback": function () {
-					QUnit.ok(window.$.fn.jquery === testVersion && $.fn.jquery !== window.$.fn.jquery, "Echo jQuery not overridden");
+					QUnit.ok(window.$.fn.jquery === testVersion && $.fn.jquery !== window.$.fn.jquery, "Checking if Echo jQuery lib wasn't overridden after another jQuery version inclusion into the page");
 
 					Echo.Loader.download({
 						"scripts": [{"url": "../third-party/jquery.pack.js"}],
 						"callback": function () {
-							QUnit.ok(window.$.fn.jquery === testVersion && $.fn.jquery !== window.$.fn.jquery, "Default jQuery at page not overridden");
+							QUnit.ok(window.$.fn.jquery === testVersion && $.fn.jquery !== window.$.fn.jquery, "Checking if the native jQuery lib on the page is not overridden after Echo jQuery inclusion");
 							window.jQuery.noConflict(true);
 							QUnit.start();
 						}
