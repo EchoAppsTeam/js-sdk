55465a6728ebc79032d018f2ad6c1c5844f727f7
Valeriy Shkiryak <lizard@aboutecho.com>
2012-08-24 14:04:25 +0400

    - fixed copy task - some files are excluded from docs stripping process - some refactoring

--- grunt.js
+++ grunt.js
@@ -1,17 +1,19 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
+	var _ = grunt.utils._;
 	var _dirs = {
-		src: "src/",
-		dest: "web/sdk/",
-		tests: "tests/"
+		src: "src",
+		dest: "web/sdk",
+		tests: "tests"
 	};
 	var _dontWrap = [
-		_dirs.src + "backplane.js",
-		_dirs.src + "loader.js",
-		_dirs.src + "third-party/jquery.js",
-		_dirs.src + "third-party/echo.jquery.noconflict.js"
+		_dirs.src + "/backplane.js",
+		_dirs.src + "/loader.js",
+		_dirs.src + "/third-party/jquery.js",
+		_dirs.src + "/third-party/echo.jquery.noconflict.js"
 	];
+	var _dontStripDocs = grunt.file.expandFiles(_dirs.src + "/third-party/*.js");
 	var _config = {
 		dirs: _dirs,
 		pkg: "<json:js-sdk.json>",
@@ -30,7 +32,7 @@ module.exports = function(grunt) {
 				" */"
 		},
 		clean: {
-			docs: ["<%= dirs.dest %>docs/"],
+			docs: ["<%= dirs.dest %>/docs"],
 			all: ["<%= dirs.dest %>"]
 		},
 		exec: {
@@ -42,9 +44,10 @@ module.exports = function(grunt) {
 		copy: {
 			js: {
 				files: {
-					"<%= dirs.dest %>": ["<%= dirs.src %>**/*.js"]
+					"<%= dirs.dest %>": ["<%= dirs.src %>/**/*.js"]
 				},
 				options: {
+					basePath: _dirs.src,
 					processContent: function(code) {
 						return grunt.helper("wrap_code", grunt.helper("strip_docs", code));
 					},
@@ -54,16 +57,19 @@ module.exports = function(grunt) {
 			"images-css": {
 				files: {
 					"<%= dirs.dest %>": [
-						"<%= dirs.src %>**/*.css",
-						"<%= dirs.src %>**/*.png",
-						"<%= dirs.src %>**/*.jpg",
-						"<%= dirs.src %>**/*.gif"
+						"<%= dirs.src %>/**/*.css",
+						"<%= dirs.src %>/**/*.png",
+						"<%= dirs.src %>/**/*.jpg",
+						"<%= dirs.src %>/**/*.gif"
 					]
+				},
+				options: {
+					basePath: _dirs.src
 				}
 			},
 			tests: {
 				files: {
-					"<%= dirs.dest %><%= dirs.tests %>": ["<%= dirs.tests %>**/*"]
+					"<%= dirs.dest %>": ["<%= dirs.tests %>/**/*"]
 				}
 			}
 		},
@@ -71,42 +77,42 @@ module.exports = function(grunt) {
 			loader: {
 				src: [
 					"<banner:meta.banner>",
-					"<%= dirs.src %>third-party/yepnope.1.5.4-min.js",
-					"<%= dirs.src %>loader.js"
+					"<%= dirs.src %>/third-party/yepnope.1.5.4-min.js",
+					"<%= dirs.src %>/loader.js"
 				],
-				dest: "<%= dirs.dest %>loader.js"
+				dest: "<%= dirs.dest %>/loader.js"
 			}
 		},
 		packs: {
 			"api": [
-				"<%= dirs.src %>api.js",
-				"<%= dirs.src %>stream-server/api.js",
-				"<%= dirs.src %>identity-server/api.js"
+				"<%= dirs.src %>/api.js",
+				"<%= dirs.src %>/stream-server/api.js",
+				"<%= dirs.src %>/identity-server/api.js"
 			],
 			"environment": [
-				"<%= dirs.src %>utils.js",
-				"<%= dirs.src %>events.js",
-				"<%= dirs.src %>labels.js",
-				"<%= dirs.src %>configuration.js",
-				"<file_strip_banner:<%= dirs.dest %>api.pack.js>",
-				"<%= dirs.src %>user-session.js",
-				"<%= dirs.src %>control.js",
-				"<%= dirs.src %>product.js",
-				"<%= dirs.src %>plugin.js",
-				"<%= dirs.src %>button.js",
-				"<%= dirs.src %>tabs.js"
+				"<%= dirs.src %>/utils.js",
+				"<%= dirs.src %>/events.js",
+				"<%= dirs.src %>/labels.js",
+				"<%= dirs.src %>/configuration.js",
+				"<file_strip_banner:<%= dirs.dest %>/api.pack.js>",
+				"<%= dirs.src %>/user-session.js",
+				"<%= dirs.src %>/control.js",
+				"<%= dirs.src %>/product.js",
+				"<%= dirs.src %>/plugin.js",
+				"<%= dirs.src %>/button.js",
+				"<%= dirs.src %>/tabs.js"
 			],
 			"third-party/jquery": [
-				"<%= dirs.src %>third-party/jquery.js",
-				"<%= dirs.src %>third-party/echo.jquery.noconflict.js",
-				"<%= dirs.src %>third-party/jquery.ihint.js",
-				"<%= dirs.src %>third-party/jquery.viewport.mini.js",
-				"<%= dirs.src %>third-party/jquery.easing-1.3.min.js",
-				"<%= dirs.src %>third-party/jquery.fancybox-1.3.4.min.js",
-				"<%= dirs.src %>third-party/echo.fancybox.css.js",
-				"<%= dirs.src %>third-party/jquery.ui-1.8.21.min.js",
-				"<%= dirs.src %>third-party/jquery.isotope.min.js",
-				"<%= dirs.src %>third-party/bootstrap/bootstrap-tab.js"
+				"<%= dirs.src %>/third-party/jquery.js",
+				"<%= dirs.src %>/third-party/echo.jquery.noconflict.js",
+				"<%= dirs.src %>/third-party/jquery.ihint.js",
+				"<%= dirs.src %>/third-party/jquery.viewport.mini.js",
+				"<%= dirs.src %>/third-party/jquery.easing-1.3.min.js",
+				"<%= dirs.src %>/third-party/jquery.fancybox-1.3.4.min.js",
+				"<%= dirs.src %>/third-party/echo.fancybox.css.js",
+				"<%= dirs.src %>/third-party/jquery.ui-1.8.21.min.js",
+				"<%= dirs.src %>/third-party/jquery.isotope.min.js",
+				"<%= dirs.src %>/third-party/bootstrap/bootstrap-tab.js"
 			]
 		},
 		//min: {
@@ -116,7 +122,13 @@ module.exports = function(grunt) {
 		//	files: ["test/**/*.html"]
 		//},
 		lint: {
-			files: ["grunt.js", "src/**/*.js", "tests/**/*.js"]
+			// TODO: exclude third-party and qunit from linting
+			// TODO: lint dest dir cause all files there are properly wrapped
+			files: [
+				"grunt.js",
+				"<%= dirs.dest %>/**/*.js",
+				"<%= dirs.tests %>/**/*.js"
+			]
 		},
 		//watch: {
 		//	files: "<config:lint.files>",
@@ -137,15 +149,17 @@ module.exports = function(grunt) {
 				browser: true
 			},
 			globals: {
-				jQuery: true
+				Echo: true,
+				jQuery: true,
+				QUnit: true
 			}
 		},
 		uglify: {}
 	};
 
-	grunt.utils._.each(["stream-server", "identity-server", "app-server"], function(name) {
-		_config.packs[name + "/controls"] = ["<%= dirs.src %>" + name + "/controls/*.js"];
-		_config.packs[name + "/plugins"] = ["<%= dirs.src %>" + name + "/plugins/*.js"];
+	_.each(["stream-server", "identity-server", "app-server"], function(name) {
+		_config.packs[name + "/controls"] = ["<%= dirs.src %>/" + name + "/controls/*.js"];
+		_config.packs[name + "/plugins"] = ["<%= dirs.src %>/" + name + "/plugins/*.js"];
 		_config.packs[name] = _config.packs[name + "/controls"].concat(_config.packs[name + "/plugins"]);
 	});
 
@@ -165,7 +179,7 @@ module.exports = function(grunt) {
 		var files = grunt.file.expandFiles(this.file.src);
 		var code = grunt.helper("wrap_and_concat", files, this.target);
 		code = grunt.template.process(grunt.config("meta.banner")) + "\n" + code;
-		grunt.file.write(grunt.config("dirs.dest") + name, code);
+		grunt.file.write(grunt.config("dirs.dest") + "/" + name, code);
 
 		if (this.errorCount) {
 			return false;
@@ -182,9 +196,17 @@ module.exports = function(grunt) {
 	// HELPERS
 	// ==========================================================================
 
-	var _stripDocsRE = /[^\r\n]*\/\*[\s\S]*?(?:\s+\* @\w)[\s\S]*?\*\/\n/g;
-	grunt.registerHelper("strip_docs", function(code) {
-		return code.replace(_stripDocsRE, "");
+	var _stripDocsRE = /[^\r\n]*\/\*[\s\S]*?\*\/\n/g;
+	grunt.registerHelper("strip_docs", function(code, filepath) {
+		if (_.include(_dontStripDocs, filepath)) {
+			return code;
+		}
+		return code.replace(_stripDocsRE, function(match) {
+			if (/\s+\* @\w/.test(match)) {
+				return "";
+			}
+			return match;
+		});
 	});
 
 	grunt.registerHelper("wrap_and_concat", function(files, name) {
@@ -194,13 +216,13 @@ module.exports = function(grunt) {
 		return files.map(function(filepath) {
 			var code = grunt.task.directive(filepath, grunt.file.read);
 			code = grunt.helper("wrap_code", code, filepath);
-			code = grunt.helper("strip_docs", code);
+			code = grunt.helper("strip_docs", code, filepath);
 			return code;
 		}).join(grunt.utils.normalizelf(grunt.utils.linefeed));
 	});
 
 	grunt.registerHelper("wrap_code", function(code, filepath) {
-		if (grunt.util._.include(_dontWrap, filepath)) {
+		if (_.include(_dontWrap, filepath)) {
 			return code;
 		}
 		if (!/third-party|\.pack\.js/.test(filepath)) {
