9869ad2da9f68bfa2b2fe3ca78cadea3e3acd325
Andrew Kushnir <kushnir.andrew@gmail.com>
2012-08-30 14:36:28 -0700

    Tools - update canvas Ruby script updated to read appkey and secret info from the local configuration file

--- tools/update-canvas.rb
+++ tools/update-canvas.rb
@@ -6,27 +6,40 @@ require 'net/https'
 require 'openssl'
 require 'json'
 
-# config
-endpoint = "https://api.echoenabled.com/v1/kvs/put"
-key = {:public => "test.aboutecho.com", :secret => "NOT.A.REAL.VALUE"}
+# check if local configuration file exists
+if (!File.exists?("../config.local.json"))
+	abort "No local configuration file (config.local.json) found..."
+end
+
+config = JSON.parse(File.read("../config.local.json"))
+
+# check if we have appkey and secret info
+if (config["appkey"] == nil || config["secret"] == nil)
+	abort "No appkey or secret found in the local config..."
+end
 
 canvas_name = ARGV.first
 canvas_file = "canvases/#{canvas_name}.json"
-abort "No canvas named '#{canvas_name}'" unless File.exists?(canvas_file)
-canvas_content = File.read(canvas_file)
+
+# check if we have corresponding canvas file
+if (!File.exists?(canvas_file))
+	abort "No canvas named '#{canvas_name}'"
+end
+
+content = File.read(canvas_file)
 
 # Validate the JSON
-JSON.parse(canvas_content)
+JSON.parse(content)
 
-uri = URI.parse(endpoint)
+uri = URI.parse("https://api.echoenabled.com/v1/kvs/put")
 
 http = Net::HTTP.new(uri.host, 443)
 http.use_ssl = true
 http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
 request = Net::HTTP::Post.new(uri.request_uri)
-request.set_form_data({:key => canvas_name, :value => canvas_content, :public => true})
-request.basic_auth(key[:public], key[:secret])
+request.set_form_data({:key => canvas_name, :value => content, :public => true})
+request.basic_auth(config["appkey"], config["secret"])
 response = http.request(request)
 
 if response.code == "200"
