5a31ec6afe3b595a1d6c14155612e9594f3c0332
Leonid Mosenkov <leon@aboutecho.com>
2012-09-01 13:38:28 +0400

    [cr, docs] text-counter ok

--- src/stream-server/plugins/text-counter.js
+++ src/stream-server/plugins/text-counter.js
@@ -1,14 +1,17 @@
+(function(){
+
 /**
  * @class Echo.StreamServer.Controls.Submit.Plugins.TextCounter
- * Adds the character counter under the text field in the Echo Submit control. Allows to set the maximum length of the text to enter.
+ * Adds the character counter under the text field in the Echo Submit control.
+ * Allows to set the maximum length of the text to enter.
  *
- *     new Echo.StreamServer.Controls.Submit({
- *         "target": document.getElementById("echo-stream"),
- *         "appkey": "test.echoenabled.com",
- *         "plugins": [{
- *             "name": "TextCounter"
- *         }]
- *     });
+ * 	new Echo.StreamServer.Controls.Submit({
+ * 		"target": document.getElementById("echo-submit"),
+ * 		"appkey": "test.echoenabled.com",
+ * 		"plugins": [{
+ * 			"name": "TextCounter"
+ * 		}]
+ * 	});
  *
  * @extends Echo.Plugin
  */
@@ -16,10 +19,14 @@ var plugin = Echo.Plugin.manifest("TextCounter", "Echo.StreamServer.Controls.Sub
 
 /**
  * @cfg {Number} limit
- * Specifies the maximum length of the text. There is no limit if it is not defined.
+ * Specifies the maximum length of the text.
+ *  There is no limit if it is not defined.
  *
  * @cfg {String} label
- * Specifies the custom label for the counter. Parameter string is a template that has several placeholders. Placeholder is some word wrapped with the curly brackets. It supports the following list of placeholder words:
+ * Specifies the custom label for the counter. Parameter string is
+ * a template that has several placeholders. Placeholder is some
+ * word wrapped with the curly brackets. It supports the following
+ * list of placeholder words:
  *
  * + limit - The number from the limit parameter. 
  * + typed - The number of characters currently typed in the text field.
@@ -31,8 +38,13 @@ var plugin = Echo.Plugin.manifest("TextCounter", "Echo.StreamServer.Controls.Sub
  * + "{typed} characters" if limit parameter is not provided
  */
 plugin.init = function() {
-	var component = this.component;
 	this.extendTemplate("insertAfter", "content", plugin.templates.counter);
+};
+
+plugin.events = {
+	"Echo.StreamServer.Controls.Submit.onPostComplete": function(topic, args) {
+		this.dom.render({"name": "counterLabel"});
+	}
 };
 
 plugin.labels = {
@@ -48,13 +60,8 @@ plugin.labels = {
 	"unlimited": "{typed} characters"
 };
 
-plugin.events = {
-	"Echo.StreamServer.Controls.Submit.onPostComplete": function(topic, args) {
-		this.dom.render({"name": "counterLabel"});
-	}
-};
-
-plugin.templates.counter = '<div class="{plugin.class:counterLabel} echo-primaryFont echo-primaryColor"></div>';
+plugin.templates.counter =
+	 '<div class="{plugin.class:counterLabel} echo-primaryFont echo-primaryColor"></div>';
 
 /**
  * @echo_renderer
@@ -82,14 +89,20 @@ plugin.component.renderers.text = function(element) {
  * @echo_renderer
  */
 plugin.renderers.counterLabel = function(element) {
-	var plugin = this;
+	var plugin = this, submit = this.component;
 	var limit = plugin.config.get("limit", 0);
-	var typed = this.component.dom.get("text").val().length;
-	var label = this.labels.get(
+	var typed = submit.dom.get("text").val().length;
+	var label = plugin.labels.get(
 		plugin.config.get("label", limit ? "limited" : "unlimited"),
-		{"typed": typed, "left": Math.max(limit - typed, 0), "limit": limit}
+		{
+			"typed": typed,
+			"left": Math.max(limit - typed, 0),
+			"limit": limit
+		}
 	);
 	return element.text(label);
 };
 
 Echo.Plugin.create(plugin);
+
+})();
