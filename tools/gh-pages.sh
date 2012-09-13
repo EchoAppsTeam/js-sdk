d711592054161ec55edc40398bda89fe07312bb7
Nikolay Safin <safin@aboutecho.com>
2012-09-13 17:32:57 +0400

    Removed obsolete code

--- tools/gh-pages.sh
+++ tools/gh-pages.sh
@@ -3,7 +3,6 @@ git checkout gh-pages
 git checkout master -- grunt.js package.json tools tests src demo
 grunt docs
 cp -r web/docs/* docs
-cp -r web/demo/* demo
 git rm -rf grunt.js package.json tools src >/dev/null 2>&1
 rm -rf web
 git add docs/ tests/ demo/
