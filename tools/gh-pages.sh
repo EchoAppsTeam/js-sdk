720c460b58d031a254b1da854f86d9187f161a0e
Valeriy Shkiryak <lizard@aboutecho.com>
2012-09-13 17:18:55 +0400

    Minor fixing in paths for Pages script

--- tools/gh-pages.sh
+++ tools/gh-pages.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-mkdir -p web/sdk
 git checkout gh-pages
-git checkout master -- tests grunt.js package.json tools src web/demo
+git checkout master -- grunt.js package.json tools tests src demo
 grunt docs
 cp -r web/docs/* docs
 cp -r web/demo/* demo
-git rm -rf grunt.js package.json tools src web/demo >/dev/null 2>&1
+git rm -rf grunt.js package.json tools src >/dev/null 2>&1
 rm -rf web
 git add docs/ tests/ demo/
 git commit -m "up"
