d7ec1ee6f3117ec1d5539e65e829c755af035838
Maksim Terekhin <neurostep@aboutecho.com>
2012-09-21 11:28:45 +0400

    [Tests] fixed typo

--- tests/unit/utils.js
+++ tests/unit/utils.js
@@ -119,7 +119,7 @@ suite.prototype.tests.TestDataMethods = {
 			"key3": "value3"
 		}, "Checking removeNestedValue() with complex key represented by Array");
 		Echo.Utils.setNestedValue(data, "key1.key-null", null);
-		QUnit.ok(!Echo.Utils.removeNestedValue(data, "key1.key-null.key"), "Check removeNestedValue() with null target");
+		QUnit.ok(!Echo.Utils.removeNestedValue(data, "key1.key-null.key"), "Checking removeNestedValue() with null target");
 
 		QUnit.equal(Echo.Utils.htmlize(), "",
 			"Checking htmlize() method with empty param");
