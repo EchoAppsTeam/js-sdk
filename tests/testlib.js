4a4a505a86ec9d11b4b59ad302abb4a9319f07a2
Valeriy Shkiryak <lizard@aboutecho.com>
2012-11-28 19:09:16 +0400

    Style fix: quotes

--- tests/testlib.js
+++ tests/testlib.js
@@ -116,7 +116,7 @@ Echo.Tests.Common.prototype.run = function() {
 
 Echo.Tests.Common.prototype.sequentialAsyncTests = function(funcs, namespace) {
 	if (namespace && $.isPlainObject(this[namespace]) && $.isFunction(this[namespace].destroy)) {
-		funcs.push('destroy');
+		funcs.push("destroy");
 	}
 	funcs.push(function() {
 		QUnit.start();
