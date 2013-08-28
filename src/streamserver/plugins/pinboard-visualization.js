7efb7bdd223d334a3e8714afaa10224eba53c1be
Maksim Terekhin <neurostep@aboutecho.com>
2013-08-28 22:37:50 +0400

    [PinboardVisualization -> Docs] Add note about reTag Stream.Item option; remove private mark from the package

--- src/streamserver/plugins/pinboard-visualization.js
+++ src/streamserver/plugins/pinboard-visualization.js
@@ -281,12 +281,18 @@ var $ = jQuery;
  * 		}]
  * 	});
  *
+ * __Note__: Since the PinboardVisualization plugin transforms Stream.Item control
+ * then item has some UI updates. Important thing is "reTag" section omit in the
+ * PinboardVisualization item template. Thus setting "reTag" configuration parameter
+ * for the Stream.Item control has no any actions. This is done to simplfy UI
+ * and avoid informational noise from the PinboardVisualization item. More information
+ * about "reTag" configuration parameter can be found [here](#!/api/Echo.StreamServer.Controls.Stream.Item-cfg-reTag).
+ *
  * More information regarding the plugins installation can be found
  * in the [“How to initialize Echo components”](#!/guide/how_to_initialize_components-section-2) guide.
  *
  * @extends Echo.Plugin
  *
- * @private
  * @package streamserver/plugins/pinboard-visualization.js
  */
 
