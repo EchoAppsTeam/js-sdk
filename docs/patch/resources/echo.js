2b9707c296edfd585cfe469595d859730e8cb72b
Valeriy Shkiryak <lizard@aboutecho.com>
2013-12-19 10:37:42 +0400

    Move docs menu without overriding the whole component

--- docs/patch/resources/echo.js
+++ docs/patch/resources/echo.js
@@ -1,41 +1,17 @@
 Docs.initEventTracking = function() {
-	Ext.getCmp('north-region').setHeight(80);
-	Ext.getCmp('north-region').items.first().setHeight(53);
+	Ext.getCmp("north-region").setHeight(80).items.first().setHeight(53);
+	var cmps = Ext.ComponentQuery.query("#north-region docheader");
+	Ext.each(cmps, function(cmp) {
+		if (cmp.menu) {
+			cmp.el.removeAllListeners().addListener("click", function() {
+				cmp.menu.showBy(cmp.el, "bl", [270, 0]);
+			});
+			return false;
+		}
+	});
 };
 
-Docs.versions = [
+Docs.otherProducts = [
 	{"text": "Echo JS SDK 3.0", "href": "http://echoappsteam.github.io/js-sdk/docs/"},
 	{"text": "Echo JS SDK 3.1", "href": "../../docs/[VERSION]"}
 ];
-
-Ext.define("Docs.view.Header", {
-	extend: "Ext.container.Container",
-	alias: "widget.docheader",
-
-	contentEl: "header-content",
-
-	initComponent: function() {
-		if (Docs.versions) {
-			this.style = "cursor: pointer;",
-			this.cls = "dropdown";
-
-			this.menu = Ext.create("Ext.menu.Menu", {
-				renderTo: Ext.getBody(),
-				plain: true,
-				items: Docs.versions
-			});
-		}
-
-		this.callParent();
-	},
-
-	listeners: {
-		afterrender: function(cmp) {
-			if (this.menu) {
-				cmp.el.addListener("click", function(cmp, el) {
-					this.menu.showBy(this.el, "bl", [270, 0]);
-				}, this);
-			}
-		}
-	}
-});
