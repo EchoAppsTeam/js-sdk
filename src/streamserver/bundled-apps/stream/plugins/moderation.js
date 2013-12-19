62fb66d6bb1f5471b65f10bba03403085f3b1b23
Ilya Mikhailov <mikhailov@aboutecho.com>
2013-12-19 18:13:54 +0400

    [Docs] added missing documentation for private class

--- src/streamserver/bundled-apps/stream/plugins/moderation.js
+++ src/streamserver/bundled-apps/stream/plugins/moderation.js
@@ -6,6 +6,19 @@ Echo.define([
 
 "use strict";
 
+/**
+ * @class Echo.StreamServer.BundledApps.Stream.ClientWidget.Plugins.Moderation
+ * Adds several moderation controls to change item status. Besides
+ * it provides the opportunity to ban specific user or change his privileges.
+ *
+ * More information regarding the plugins installation can be found
+ * in the [“How to initialize Echo components”](#!/guide/how_to_initialize_components-section-initializing-plugins) guide.
+ *
+ * @extends Echo.Plugin
+ *
+ * @private
+ * @package streamserver.sdk.js
+ */
 var plugin = Plugin.definition("Moderation", "Echo.StreamServer.BundledApps.Stream.ClientWidget");
 
 plugin.events = {
