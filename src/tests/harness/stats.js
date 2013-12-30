7f1b0ca542bdd7bbfd4d89b75d862b59507a4b89
Valeriy Shkiryak <lizard@aboutecho.com>
2013-12-30 12:28:28 +0400

    Sort stats list only if available

--- src/tests/harness/stats.js
+++ src/tests/harness/stats.js
@@ -309,7 +309,7 @@ function showCoverageList(type, prefix) {
 	var html = "", data = [];
 	var el = $(".echo-tests-stats-info");
 	var list = _coverage[prefix].processed[type];
-	list.sort();
+	list && list.sort();
 	_isListVisible = !(_isListVisible && _lastListType === prefix + "-" + type);
 	if (_isListVisible) {
 		if (prefix === "functions") {
