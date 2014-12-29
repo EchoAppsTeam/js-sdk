c3569ebba86a56b4a3faba25f4faa62f55a2eb45
Dmitry Bragin <dbragin@aboutecho.com>
2014-12-29 22:40:04 +0300

    We should normalize only layout of the canvas

--- src/canvas.js
+++ src/canvas.js
@@ -358,7 +358,7 @@ canvas.methods.updateLayout = function(apps, layout) {
 		this.render();
 		return apps;
 	};
-	this.set("data.apps", apps || []);
+	this.set("data.apps", apps);
 	this.set("data.layout", layout || []);
 	return Echo.Utils.pipe([
 		$.proxy(this._loadAppResources, this, apps),
